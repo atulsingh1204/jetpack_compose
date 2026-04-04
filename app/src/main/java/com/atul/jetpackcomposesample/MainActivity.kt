d79efbb04423a2b633ac0c7bb4c50aae1ffdfdf1
Atul <atulksingh0001@gmail.com>
2026-04-05 01:36:19 +0530

    Initial commit

---
+++ app/src/main/java/com/atul/jetpackcomposesample/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.atul.jetpackcomposesample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.atul.jetpackcomposesample.ui.theme.JetPackComposeSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetPackComposeSampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+
+                MyScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun MyScreen() {
+    Column {
+        Row {
+            Text("This is just for testing")
+            Button(onClick = {}) {
+                Text("Click Me")                    
+            }
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetPackComposeSampleTheme {
+        Greeting("Android")
+    }
+}
