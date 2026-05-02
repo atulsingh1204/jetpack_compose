4160c8a182a665437bfcca927210657e44d0b274
Atul <atulksingh0001@gmail.com>
2026-05-02 14:18:29 +0530

    Implemented a button

--- app/src/main/java/com/atul/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/atul/jetpackcomposesample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.atul.jetpackcomposesample
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,7 +14,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.atul.jetpackcomposesample.ui.theme.JetPackComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +25,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-            Text(text = "Hello Atul")
+//            MyUI()
+//            Greeting()
+
+            MyScreen()
         }
 
+    }
+
+    @Composable
+    fun MyUI(){
+        Text("Hello")
+    }
+
+    @Composable
+    fun Greeting(){
+        Text("Hello Atul")
+    }
+
+    @Composable
+    fun MyScreen(){
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+        ) {
+            Text("Hello")
+            Text("Welcome to Compose")
+
+            Button(onClick = { Toast.makeText(this@MainActivity, "Button is clicked", Toast.LENGTH_SHORT).show()}){
+                Text("Click Me")
+            }
+        }
     }
 }
 
