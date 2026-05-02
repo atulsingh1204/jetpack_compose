8e9feadac0dfb568d4d4c8703c948e529f239d5f
Atul <atulksingh0001@gmail.com>
2026-05-02 22:27:47 +0530

    Learned about text, button and textfield

--- app/src/main/java/com/atul/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/atul/jetpackcomposesample/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.atul.jetpackcomposesample
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ListItemDefaults.contentColor
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.atul.jetpackcomposesample.ui.theme.JetPackComposeSampleTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,39 +29,91 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-//            MyUI()
-//            Greeting()
+//            PreviewFunctions() //Text
+//            ImageFunctions()
+//            ButtonFunction()
+//            PreviewTextField()
 
-            MyScreen()
+            TextInput()
+
         }
 
     }
 
+//    @Preview(showBackground = true, widthDp = 300, heightDp =  500)
+//    @Composable
+//    private fun PreviewFunctions(){
+//        Text(
+//            text = "Hello Atul",
+//            fontStyle = FontStyle.Italic,
+//            fontWeight = FontWeight.ExtraBold,
+//            color = Color.Red,
+//            fontSize = 30.sp,
+//            textAlign = TextAlign.Center,
+//        )
+//    }
+
+//    @Preview(showBackground = true, widthDp = 300, heightDp =  300)
+//    @Composable
+//    private fun ImageFunctions(){
+//        Image(
+//            painter = painterResource(id = R.drawable.ic_outline_arrow_shape_up_stack),
+//            contentDescription = "Dummy image",
+//            colorFilter = ColorFilter.tint(Color.Blue),
+//            contentScale = ContentScale.Crop
+//        )
+//    }
+
+
+//@Preview(showBackground = true, widthDp = 300, heightDp =  300)
+//@Composable
+//private fun ButtonFunction(){
+//    Button(
+//        onClick = {
+//            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+//        },
+//        colors = ButtonDefaults.buttonColors(
+//            Color.Green,
+//            Color.Black
+//        ),
+//        enabled = false
+//    ){
+//        Text("Hello", color = Color.Blue, fontSize = 50.sp)
+//        Image(
+//            painter = painterResource(id = R.drawable.ic_outline_arrow_shape_up_stack),
+//            contentDescription = "Dummy Image"
+//        )
+//    }
+//}
+
+
+    @Preview(showBackground = true, widthDp = 300, heightDp =  300)
     @Composable
-    fun MyUI(){
-        Text("Hello")
+    private fun PreviewTextField(){
+        TextField(
+            value = "Hello Atul",
+            onValueChange = {
+                Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+            },
+            label = {Text("Enter your name")},
+            placeholder = {Text("Name")},
+        )
+
     }
 
     @Composable
-    fun Greeting(){
-        Text("Hello Atul")
+    fun TextInput(){
+        val state = remember{ mutableStateOf("") }
+        TextField(
+            value = state.value,
+            onValueChange = {
+                state.value = it
+//                Log.e("check", "TextInput: " + it)
+            },
+            label = {Text("Enter Message")},
+        )
     }
 
-    @Composable
-    fun MyScreen(){
-
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp)
-        ) {
-            Text("Hello")
-            Text("Welcome to Compose")
-
-            Button(onClick = { Toast.makeText(this@MainActivity, "Button is clicked", Toast.LENGTH_SHORT).show()}){
-                Text("Click Me")
-            }
-        }
-    }
 }
 
 
